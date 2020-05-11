3209285c497800b37071ad8bd32aa9cbd6521f73
Joel Garcia <joel.garciajr84@gmail.com>
2020-05-10 21:01:14 -0300

    Fix author string

---
+++ index.js
@@ -0,0 +1,156 @@
+const Alexa = require('ask-sdk-core');
+const https = require('https');
+
+const quoteID = function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+
+const getFrasesMotivacionais = function () {
+  return new Promise((resolve, reject) => {
+    const request = https.get(
+      'https://raw.githubusercontent.com/joelgarciajr84/alexa-frases-inspiracao/master/quotes.json',
+      (response) => {
+        response.setEncoding('utf8');
+
+        let returnData = '';
+        if (response.statusCode < 200 || response.statusCode >= 300) {
+          return reject(
+            new Error(
+              `${response.statusCode}: ${response.req.getHeader('host')} ${
+                response.req.path
+              }`
+            )
+          );
+        }
+
+        response.on('data', (chunk) => {
+          returnData += chunk;
+        });
+
+        response.on('end', () => {
+          console.log('END -> ', returnData);
+          resolve(returnData);
+        });
+
+        response.on('error', (error) => {
+          reject(error);
+        });
+      }
+    );
+    request.end();
+  });
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    const quotes = JSON.parse(await getFrasesMotivacionais());
+    const id = quoteID(0, quotes.length);
+    const quoteToSay = `${quotes[id].quote} de ${quotes[id].author}`;
+    return handlerInput.responseBuilder.speak(quoteToSay).getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent'
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = 'You can say hello to me! How can I help?';
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' &&
+      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        'AMAZON.CancelIntent' ||
+        Alexa.getIntentName(handlerInput.requestEnvelope) ===
+          'AMAZON.StopIntent')
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = 'Goodbye!';
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) ===
+      'SessionEndedRequest'
+    );
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+    );
+  },
+  handle(handlerInput) {
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const speakOutput = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
+        .speak(speakOutput)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.stack}`);
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
